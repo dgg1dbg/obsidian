61f287588492f2f884a550f43513fd925b1d94e2
dgg1dbg <jeongmin.lihi@gmail.com>
2026-03-01 16:34:52 +0900

    deploy: github pages with submodules

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "dgg1dbg.github.io/obsidian",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
